cbea4acf9cfe9827a54275dcdf29cf90305e3395
akhilesh tiwari <akhilesh.tiwari10@gmail.com>
2019-05-01 17:02:01 -0400

    enabled api caching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,40 +8,18 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
 
-  
+
   title = 'app';
 
-  constructor(private http: HttpClient){
+  constructor(private http: HttpClient) {
 
   }
 
   ngOnInit(): void {
-    this.http.get("http://localhost:3000/posts").subscribe(res =>{
+    this.http.get("http://localhost:3000/posts").subscribe(res => {
       console.log(res);
     });
   }
 
-  requestNotification(){
-    Notification.requestPermission().then(result => {
-      console.log("result:",result);
-      if(result === 'granted'){
-        this.randomNotification();
-      }
-    });
-  }
-
-  randomNotification() {
-    var randomItem = Math.floor(Math.random());
-    var notifTitle = 'Akhilesh Notification';
-    var notifBody = 'Created by Akhilesh';
-    var notifImg = '/favicon.ico';
-    var options = {
-        badge: notifImg,
-        body: notifBody,
-        icon: notifImg,
-        image: notifImg
-    }
-    var notif = new Notification(notifTitle, options);
-    setTimeout(this.randomNotification, 30000);
-}
+  
 }
